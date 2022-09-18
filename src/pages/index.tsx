ea1d44761360866d462e6ddda5d2930ed8a70037
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-18 21:19:12 +1000

    feat: Update structure and files

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className='mx-auto flex flex-col justify-center items-center'>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
